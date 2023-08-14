832aed33d996272b96da616ad2d80b42b2dbab6c
kamui <lannguyenthanhduy@gmail.com>
2023-08-14 12:01:53 +0700

    123

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
           <h3 class="card-title">Login</h3>
         </div>
         <div class="card-body">
-          <a href="login.php"> <input type="image" img src="https://github.com/superman98888/loginform/blob/main/ok1.gif" alt="login image" width="53%" height="53%"></a>
+          <a href="login.php"> <input type="image" img src="ok1.gif" alt="login image" width="53%" height="53%"></a>
           <p class="card-text text-muted">Never share your password with anyone.</p>
           <a href="login.php" class="btn btn-primary"> Sign In</a>                                    
         </div>
@@ -25,7 +25,7 @@
           <h3 class="card-title">Register</h3>
         </div>
         <div class="card-body">
-          <a href="login.php"><img src="images/registration.png" alt="register image" width="54%" height="54%"></a>
+          <a href="login.php"><img src="1.png" alt="register image" width="54%" height="54%"></a>
           <p class="card-text text-muted">Always use different password for different site.</p>
           <a href="register.php" class="btn btn-primary"> Sign Up </a>
         </div>
