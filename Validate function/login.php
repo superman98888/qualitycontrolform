26e29f0b1e06b77088b3b0209ed9a8a30946338a
kamui <lannguyenthanhduy@gmail.com>
2023-08-14 15:01:39 +0700

    123

--- Validate function/login.php
+++ Validate function/login.php
@@ -1,19 +1,19 @@
 <div class="container col-4 border rounded bg-light mt-5" style='--bs-bg-opacity: .5;'>
-  <h1 class="text-center">Sign In</h1>
+  <h1 class="text-center">Đăng Nhập</h1>
   <hr>
   <form action="" method="post">
     <div class="mb-3">
-      <label for="email" class="form-label">Email ID</label>
-      <input type="email" class="form-control" name="email" placeholder="Enter your email" autocomplete="off" required>
-      <small class="text-muted">Your email is safe with us.</small>
+      <label for="email" class="form-label">Tên đăng nhập </label>
+      <input type="email" class="form-control" name="email" placeholder="Nhập tên đăng nhập của bạn ở đây" autocomplete="off" required>
+      <small class="text-muted">Kiểm tra kĩ tên đăng nhập của bạn</small>
     </div>
     <div class="mb-3">
-      <label for="password" class="form-label">Password</label>
-      <input type="password" class="form-control" name="password" placeholder="Enter your password" required>
-      <small class="text-muted">Do not share your password.</small>
+      <label for="password" class="form-label">Mật khẩu </label>
+      <input type="password" class="form-control" name="password" placeholder="Nhập mật khẩu của bạn ở đây" required>
+      <small class="text-muted">Vui lòng không chia sẻ mật khẩu</small>
     </div>
     <div class="mb-3">
-      <input type="submit" name="signin" value="Sign In" class="btn btn-primary">
+      <input type="submit" name="signin" value="Đăng nhập" class="btn btn-primary">
     </div>
   </form>
 </div>
@@ -22,10 +22,10 @@
 <?php
 
 if (isset($_POST['signin'])) {
-  $email = $_POST['email'];
+  $user_name = $_POST['username'];
   $password = $_POST['password'];
 
-  $query = "SELECT * from users WHERE email = '$email' AND password = '$password'";
+  $query = "SELECT * from users WHERE username = '$user_name' AND password = '$password'";
   $user = mysqli_query($conn, $query);
 
   if (!$user) {
@@ -45,7 +45,7 @@ if (isset($_POST['signin'])) {
     $_SESSION['name'] = $user_name;   // Storing the value in session
     $_SESSION['email'] = $user_email; // Storing the value in session
     //! Session data can be hijacked. Never store personal data such as password, security pin, credit card numbers other important data in $_SESSION
-    header('location: dashboard.php?user_id=' . $user_id);
+    header('location: ../dashboard.php?user_id=' . $user_id);
   } else {
     header('location: login.php'); 
   }
